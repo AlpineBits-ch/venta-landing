872f3b8e501ab641b974b02d77c37a36cf465496
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-17 23:08:19 +0200

    iT sOuNdS lIkE aI

--- src/app/blog-service.ts
+++ src/app/blog-service.ts
@@ -16,7 +16,8 @@ export class BlogService {
 
 ## Origins of Venta.GG
 
-It started not with a social app, but with a game. We're building something called *Fractured Echoes*. A fractured ring world, where the single shards are drifting away into a void creating a very interesting gameplay for everyone who loves such games. We needed a voice and social platform to go with it and for us trying to build that platform first sounded like a good idea, since we can figure out an architecture and a core server stack and then build our game networking around that.
+It started not with a social app, but with a game. We're building an MMO, where in game socials and voice comms play a cruical role and therefore
+ we needed a voice and social platform to go with it and for us trying to build that platform first sounded like a good idea, since we can figure out an architecture and a core server stack and then build our game networking around that.
 And if we're lucky, we can cross fund our game development with that platform. Paying creative people who do the assets and sounds costs money, after all.
 
 ## Where are we now?
