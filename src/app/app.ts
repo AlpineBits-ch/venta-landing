5af0773239680582045694138c81778dbb464cdc
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-17 12:15:36 +0200

    feat: add stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, HostListener, signal} from '@angular/core';
 import {Header} from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('alpine-angular-landing');
+  isOpen = false;
+
+  toggleDropdown(event: MouseEvent) {
+    event.stopPropagation();
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
+
+  @HostListener('document:click')
+  onDocumentClick() {
+    this.closeDropdown();
+  }
 }
