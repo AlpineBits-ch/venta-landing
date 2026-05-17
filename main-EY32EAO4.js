b6c2eaf48438c279991045c27ba29464d8920058
dominic-AlpineBits <dominic@alpinebits.ch>
2026-05-17 21:18:21 +0000

    Deploying to gh-pages from @ AlpineBits-ch/venta-landing@18957bc4b31c05fddceb19e7ed004189ed6ff5d5 🚀

--- main-EY32EAO4.js
+++ main-EY32EAO4.js
@@ -6,8 +6,8 @@ ${n.map((r,o)=>`${o+1}) ${r.toString()}`).join(`
 
 ## Origins of Venta.GG
 
-It all started with a game and not a social app.<br> We're building an MMO, where in game socials and voice comms play a cruical role and therefore
- we needed a voice and social platform to go with it.<br>
+The beginning of what now became "Venta", started with the idea of creating a video game called <insert name here :D> about a fractured ring world drifting in space. Bla bla bla...
+While brainstorming and working on the concept of our game, we realized that we needed a voice and social platform to go with it.<br>
  For us, trying to build that platform first sounded like a good idea, since we can figure out an architecture and a core server stack and then build our game networking around that. <br>
 And if we're lucky, we can cross fund our game development with that platform. Paying creative people who do the assets and sounds costs money, after all.
 
